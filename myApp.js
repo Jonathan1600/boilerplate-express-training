835263aba8e29cd3b78f91ea46015c5070f2a974
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2020-11-22 00:43:01 -0700

    Implementing Request Logger and .env ussage

--- myApp.js
+++ myApp.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  } );
+    app.use((req, res, next) => {
+        console.log(req.method + " " + req.path + " - " + req.ip);
+        next();
+    });
+    
+    app.get("/", function (req, res) {
+        res.sendFile(__dirname + "/views/index.html");
+    });
 
-  app.use(express.static(__dirname + "/public"));
-  
-  app.get("/json", (req, res) => {
-    res.json({"message": "Hello json"});
-  });
+    app.use(express.static(__dirname + "/public"));
 
-
-
-
-
-
-
+    app.get("/json", (req, res) => {
+        res.json({ "message": "Hello json" });
+    });
 
 
 
@@ -44,4 +42,12 @@ app.get("/", function(req, res) {
 
 
 
- module.exports = app;
+
+
+
+
+
+
+
+
+module.exports = app;
