d2f8db40609b6a6b20c2e9566d742954f09efc0d
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2020-11-22 21:40:16 -0700

    Implementing body parser

--- myApp.js
+++ myApp.js
@@ -1,41 +1,49 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-    app.use((req, res, next) => {
-        console.log(req.method + " " + req.path + " - " + req.ip);
-        next();
+  app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+  });
+
+  app.use(
+    bodyParser.urlencoded({extended: true})
+  );
+
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  } );
+
+  app.use(express.static(__dirname + "/public"));
+  
+  app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") { 
+      res.json({"message": "HELLO JSON"});
+    } else {
+      res.json({"message": "Hello json"});
+    }
+  });
+
+  app.get('/now', (req, res, next) => {
+    console.log("middleware");
+    next();
+  }, (req, res) => {
+    res.json({"time": new Date().toString()})
+  });
+
+  app.get("/:word/echo", (req, res) => {
+    res.json({echo: req.params.word})
+  });
+
+  app.route("/name").get((req, res) => {
+    res.json({name: `${req.query.first} ${req.query.last}`})
     });
 
-    app.get("/", function(req, res) {
-        res.sendFile(__dirname + "/views/index.html");
-    } );
 
-    app.use(express.static(__dirname + "/public"));
-    
-    app.get("/json", (req, res) => {
-        if (process.env.MESSAGE_STYLE === "uppercase") { 
-        res.json({"message": "HELLO JSON"});
-        } else {
-        res.json({"message": "Hello json"});
-        }
-    });
 
-    app.get('/now', (req, res, next) => {
-        console.log("middleware");
-        next();
-    }, (req, res) => {
-        res.json({"time": new Date().toString()})
-    });
 
-    app.get("/:word/echo", (req, res) => {
-        res.json({echo: req.params.word})
-      });
 
-    app.get("/name", (req, res) => {
-        let firstName = req.query.first;
-        let lastName = req.query.last;
-        res.json({ name: `${firstName} ${lastName}`});
-    });  
 
 
 
